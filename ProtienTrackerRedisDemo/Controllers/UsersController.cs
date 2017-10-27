70d81aa8e8d5e2516fd87872bc874d7026e5d4e5
MilindaSeneviratne <milinda.seneviratne@chilindo.com>
2017-10-27 17:16:31 +0700

    Added a controller to save user infomration.

---
+++ ProtienTrackerRedisDemo/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using ProtienTrackerRedisDemo.Models;
+using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProtienTrackerRedisDemo.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult NewUser()
+        {
+            return View();
+        }
+
+        public ActionResult Save(string userName, int goal)
+        {
+            using (IRedisClient client = new RedisClient(new RedisEndpoint { Host = "117.20.40.28", Port = 6379, Password = "Yellow889" }))
+            {
+                var userClient = client.As<User>();
+                var user = new User
+                {
+                    Name = userName,
+                    Goal = goal,
+                    Id = userClient.GetNextSequence()
+                };
+
+                userClient.Store(user);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
