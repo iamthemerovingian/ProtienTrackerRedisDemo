c0e16210b339a3dbe61849d065e5b4d947c3e744
MilindaSeneviratne <milinda.seneviratne@chilindo.com>
2017-10-27 16:29:40 +0700

    Added a home controller which gets all the users from redis and displays it in the index view.

---
+++ ProtienTrackerRedisDemo/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using ProtienTrackerRedisDemo.Models;
+using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProtienTrackerRedisDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            using (IRedisClient client = new RedisClient(new RedisEndpoint { Host = "117.20.40.28", Port = 6379, Password = "Yellow889" }))
+            {
+                var userClient = client.As<User>();
+                var users = userClient.GetAll();
+                var userSelection = new SelectList(users, "Id", "Name", string.Empty);
+
+                ViewBag.Users = userSelection;
+            }
+            return View();
+        }
+    }
+}
